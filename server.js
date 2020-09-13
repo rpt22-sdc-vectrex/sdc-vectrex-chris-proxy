642a6f7395fb64e143cdf861e4490a068d2c4cd7
Rita Rozsavolgyi <rvrita.me@gmail.com>
2020-09-13 01:30:30 -0700

    Proxy setup, 4 modules + header, footer

--- server.js
+++ server.js
@@ -13,20 +13,28 @@ app.use(cors());
 
 const proxyRoutes = [
   {
+    paths: [ '/header-bundle.js' ],
+    server: 'http://etsy-header.rvrita.com/',
+  },
+  {
+    paths: [ '/footer-bundle.js' ],
+    server: 'http://etsy-footer.rvrita.com/',
+  },
+  {
     paths: [ '/reviews/*', '/review-summary/*', '/review-list/*', '/reviews-pictures/*', '/reviews-bundle.js' ],
     server: 'http://etsy-reviews.rvrita.com',
   },
   {
-    paths: [ '/itemDetails/*', '/info', '/bundle.js' ],
-    server: 'http://localhost:5000',
+    paths: [ '/itemDetails/*', '/info/*', '/bundle.js' ],
+    server: 'http://ec2-3-133-108-106.us-east-2.compute.amazonaws.com',
   },
   {
-    paths: [ '/pictures', '/pictures/*', '/reviewPhotos/*', '/main.bundle.js' ],
-    server: 'http://localhost:3000',
+    paths: [ '/pictures', '/pictures/*', '/reviewPhotos/*', '/pictures.bundle.js' ],
+    server: 'http://13.56.229.226',
   },
   {
     paths: [ '/shipping-api/*', '/dist/ttreit-shipping.js' ],
-    server: 'http://3.93.215.160',
+    server: 'http://3.95.162.236',
   },
 ];
 
@@ -47,6 +55,8 @@ proxyRoutes.forEach(route => {
     app.get(path, handler);
   });
 });
+
+app.use(express.static(__dirname + '/public')); 
 
 // fall through to index.html
 app.get('*', (req, res) => {
